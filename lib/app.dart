59291fa187c24d3fcdc89f92eb9235de42e5a282
HP <susandanne@gmail.com>
2024-02-24 19:21:27 +0600

    Get Started With UI design

---
+++ lib/app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager_project_in_flutter/presentation/screens/splash_screens.dart';
+import 'package:task_manager_project_in_flutter/presentation/utilitites/Colors_App.dart';
+
+class TaskManager extends StatelessWidget {
+  const TaskManager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title:'tast manager',
+      home: Splash_Screens(),
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+              fillColor: Colors.grey,
+              filled: true,
+              contentPadding: EdgeInsets.all(10),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(8),
+              )
+          ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape:RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8)
+            ),
+            backgroundColor:AppColor.ThemeColor,
+            foregroundColor: Colors.green
+          )
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.red,
+            // backgroundColor: Colors.blue
+          )
+        )
+        )
+    );
+  }
+}
